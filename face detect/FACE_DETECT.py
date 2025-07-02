2d6b3f8c25ad042b88d784d0be35e7e51eed6559
Muhammad Ammar <muh.ammar02@gmail.com>
2025-07-03 02:25:17 +0500

    added notes to better edit later

--- face detect/FACE_DETECT.py
+++ face detect/FACE_DETECT.py
@@ -4,15 +4,17 @@ facedetect = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalf
 
 while True:
     ret, frame = video.read()
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)  # Convert to grayscale
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)  # black and white conversion
     faces = facedetect.detectMultiScale(gray, 1.3, 5)
     for x, y, w, h in faces:
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 3)
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 3) # draw rectangle around face
         cv2.putText(frame, "Face", (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 0, 0), 2)
-    cv2.imshow("WindowFrame", frame)
+    cv2.imshow("WindowFrame", frame) # name of frame
     k = cv2.waitKey(1)
     if k == ord('q'):
         break
+
+
 
 video.release()
 cv2.destroyAllWindows()
